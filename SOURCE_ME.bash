f3317dc84661aaefd9bdd3bc6f983f5e3112ba6e
Hiroyuki Sano <sh19910711@gmail.com>
2015-10-13 14:00:23 +0900

    chore: edit source_me

--- SOURCE_ME.bash
+++ SOURCE_ME.bash
@@ -1,6 +1,6 @@
 source_me() {
   local root=$(dirname ${BASH_SOURCE})
-  for f in $(find ${root} -type f -name '*.bash'); do
+  for f in $(find ${root}/src -type f -name '*.bash'); do
     if [[ "$(basename ${f})" != "$(basename ${BASH_SOURCE})" ]]; then
       source ${f}
     fi
