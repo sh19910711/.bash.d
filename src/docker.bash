acea3c46708bcb4c829c7c426e07ea9846e5c797
Hiroyuki Sano <sh19910711@gmail.com>
2015-11-26 15:36:53 +0900

    feat(src): set uid/gid to docker_run_with_git()

--- src/docker.bash
+++ src/docker.bash
@@ -2,5 +2,5 @@
 docker_run_with_git() {
   local root=$(git rev-parse --show-toplevel)
   local relpath=$(realpath --relative-base ${root} $(pwd))
-  sudo docker run -w /workspace/${relpath} -v ${root}:/workspace -ti $@
+  sudo docker run -u ${UID}:${GID} -w /workspace/${relpath} -v ${root}:/workspace -ti $@
 }
