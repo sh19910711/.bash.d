19eb88ef87f292e0007dc27e9351dc62324394b4
Hiroyuki Sano <sh19910711@gmail.com>
2015-12-04 16:34:07 +0900

    add(src): add bashrc

--- source_me.bash
+++ source_me.bash
@@ -1,5 +1,6 @@
 source_me() {
   local root=$(dirname ${BASH_SOURCE})
+  source ${root}/src/bashrc
   for f in $(find ${root}/extsrc ${root}/src -type f -name '*.bash'); do
     if [[ "$(basename ${f})" != "$(basename ${BASH_SOURCE})" ]]; then
       source ${f}
